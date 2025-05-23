2710a99f4a22201cfe86eea47d3e23705a7337dc
DESKTOP-FIDOEO4\user <jungheon21@naver.com>
2025-05-23 22:36:12 +0900

    setting

---
+++ individual/src/main/java/com/my/individual/TestController.java
@@ -0,0 +1,11 @@
+package com.my.individual;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public void first(){
+        System.out.println("first");
+    }
+}
